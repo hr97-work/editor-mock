0f9950b76f2edfcfb78da421275651741f6b1799
hr97-work <hr.petrosyan.dev@gmail.com>
2024-12-24 19:14:09 +0400

    Update runCodeService.js

--- src/services/runCodeService.js
+++ src/services/runCodeService.js
@@ -1,5 +1,5 @@
 export const runCodeService = async (code, language) => {
-  const endpoint = `http://localhost:3001/results?code=${encodeURIComponent(code)}&language=${language}`;
+  const endpoint = `${import.meta.env.VITE_APP_MOCK_URL}/results?code=${encodeURIComponent(code)}&language=${language}`;
   const response = await fetch(endpoint, {
     headers: {
       'Content-Type': 'application/json',
